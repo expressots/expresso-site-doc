7a138ecbaa465db40836bf0360c34a66566afe76
Richard Zampieri <rc_scott48@hotmail.com>
2023-02-24 20:25:41 -0800

    fix: update docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'expresso-ts.com', //'https://expressots.github.io/',
+  url: 'https://expresso-ts.com', //'https://expressots.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/expresso-site-doc/',
