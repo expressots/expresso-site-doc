dee83cff54b5b832a80fe6b7e9055efd8aaa424f
Richard Zampieri <rc_scott48@hotmail.com>
2023-02-24 18:36:08 -0800

    fix: project name issue

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'expressots', // Usually your GitHub org/user name.
-  projectName: 'expressots', // Usually your repo name.
+  projectName: 'expresso-site-doc', // Usually your repo name.
   deploymentBranch: "gh-pages",
 
   onBrokenLinks: 'warn', // 'throw' | 'warn' | 'ignore'
